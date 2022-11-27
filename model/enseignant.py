170426e0c88a8166e25c0c81f32bfc4b3b8c32cf
MiraMimi1 <56454854+MiraMimi1@users.noreply.github.com>
2022-11-27 22:57:24 +0100

    Delete model directory

--- model/enseignant.py
+++
@@ -1,8 +0,0 @@
-#classe enseignant
-class enseignant(personne):
-    def __init__(self, nom:str, prenom:str, date_de_naissance:str, lieu_de_naissance:str, adresse_electronique:str, matricule:int):
-        super().init(nom:str, prenom:str, date_de_naissance:str, lieu_de_naissance:str, adresse_electronique:str, matricule:int)
-
-    #additional getters
-
-    #additional setters
