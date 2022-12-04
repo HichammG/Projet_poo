88f0a9e33e56e7408ba38d7bd02de65fbfff2498
REKKACHE Amira <56454854+MiraMimi1@users.noreply.github.com>
2022-12-04 21:31:14 +0100

    Update Administrateur.py

--- Administrateur.py
+++ Administrateur.py
@@ -19,40 +19,14 @@ class Administrateur(Personne):
     # setters
     def set_mot_de_pass(self, mot_de_passe):
         self.__mot_de_pass = mot_de_passe
-
-    # Connexion a l application via un login et un mot de passe
-    def connect(self, login: str, mot_de_passe: str):
-        # fonction de connexion
-        pass
-
-    def create_in_DB (self):
-        #connect to DB
-        cnx = mysql.connector.connect(user='root', password='password', host='127.0.0.1', database='poo')
-        cursor = cnx.cursor()
-#        var=(f"INSERT INTO admin (nom, prenom, mail, date_naissance, lieu_naissance,password ) values ({self.get_nom()}, {self.get_prenom()}, {self.get_adresse_electronique()}, {self.get_date_de_naissance()}, {self.get_lieu_de_naissance()}, {self.get_mot_de_pass()});")
-        res=cursor.execute("SELECT * FROM admin")
-#        cnx.commit()
-#        cursor.close()
-        cnx.close()
-        print(res)
-        print("Admin object created in database")
-
-    def read_from_DB(self, matricule):
-        # connect to DB
-        cnx = mysql.connector.connect(user='root', password='password', host='127.0.0.1', database='poo')
-        cursor = cnx.cursor()
-        res = cursor.execute("SELECT * FROM admin")
-
-        cnx.close()
-        print(res)
-        print("Admin object read from database")
-
-
+        
+    #creer une classe
     def create_classroom(self,name):
         c=classe(name)
         sql2=f"INSERT INTO poo.classroom (Nom) VALUES ('{name}')"
         db_management.insert_in_db(sql2)
 
+    #ajouter un etudiant ou bien un enseignant à la classe selon le choix
     def ajouter_personne_classe(self,choix_type, classe_ID_input, personne_ID_choix):
         if choix_type=='1':
             sql=f"INSERT INTO poo.class_enseignant VALUES ('{classe_ID_input}', '{personne_ID_choix}')"
@@ -62,7 +36,8 @@ class Administrateur(Personne):
             sql=f"UPDATE poo.etudiant SET class_ID={classe_ID_input} WHERE etudiant.ID={personne_ID_choix}"
             db_management.insert_in_db(sql)
             print("Personne ajoutée à la classe")
-        
+     
+    #afficher les enseignants et les etudiants d'une classe
     def afficher_classe(self, classe_ID_input):
         sql=f"SELECT nom, prenom from poo.etudiant WHERE class_ID='{classe_ID_input}'"
         etudiants=db_management.query_from_DB(sql)
@@ -73,6 +48,7 @@ class Administrateur(Personne):
         print("les etudiants de cette classe sont:")
         print(etudiants)
 
+    #supprimer un étudiant ou bien un enseignant d'une classe
     def retirer_personne_classe(self,choix_type,classe_ID_input,personne_ID_choix):
         if choix_type=='1':
             sql=f"DELETE FROM poo.class_enseignant WHERE (class_ID='{classe_ID_input}'AND enseignant_ID='{personne_ID_choix}'"
@@ -82,7 +58,8 @@ class Administrateur(Personne):
             sql=f"UPDATE poo.etudiant SET class_ID=NULL WHERE etudiant.ID={personne_ID_choix}"
             db_management.insert_in_db(sql)
             print("Personne retirée de la classe")
-
+            
+    #modifier les informations d'un enseignant ou d'un étudiant
     def modifier_personne(self,choix_type,personne_ID_choix, column_input, val_input):
         if choix_type=='1':
             sql=f"UPDATE poo.enseignant SET {column_input}='{val_input}' WHERE ID='{personne_ID_choix}'"
@@ -92,12 +69,13 @@ class Administrateur(Personne):
             sql=f"UPDATE poo.etudiant SET {column_input}='{val_input}' WHERE ID='{personne_ID_choix}'"
             db_management.insert_in_db(sql)
             print("Personne modifiée")
-    
+            
+    #afficher les classes dans lesquelles un enseignant aura à intervenir
     def visualiser_classes_enseignant(self,personne_ID_choix):
         sql=f"SELECT nom FROM poo.classroom WHERE ID=(SELECT class_ID from poo.class_enseignant WHERE enseignant_ID='{personne_ID_choix}')"
         res=db_management.query_from_DB(sql)
         print(res[0][0])
-
+    #afficher la salle de classe d’un étudiant ainsi que la liste de ses collègues
     def afficher_classe_etudiant(self,personne_ID_choix):
         sql=f"SELECT nom, prenom from poo.etudiant where class_ID=(SELECT class_ID from poo.etudiant WHERE ID='{personne_ID_choix}')"
         etudiants=db_management.query_from_DB(sql)
