d516e89dae1778ec6ca518494e3dd6e61c9bb0c2
REKKACHE Amira <56454854+MiraMimi1@users.noreply.github.com>
2022-12-04 21:22:20 +0100

    Update Enseignant.py

--- Enseignant.py
+++ Enseignant.py
@@ -13,7 +13,8 @@ class Enseignant (Personne):
 
     def setNumero_Tel(self, numero):
         self.__numero_tel = numero
-
+        
+    # visualiser les classes dans lesquelles l'enseignant aura à intervenir
     def visualiser_classes_enseignant(self,personne_ID_choix):
         sql=f"SELECT nom FROM poo.classroom WHERE ID=(SELECT class_ID from poo.class_enseignant WHERE enseignant_ID='{personne_ID_choix}')"
         res=db_management.query_from_DB(sql)
@@ -21,12 +22,14 @@ class Enseignant (Personne):
             print(res[0][0])
         else: 
             print ("[]")
-
+      
+    # visualiser les étudiants d’une salle de classe
     def visualiser_classe(self, choix_classe):
         sql = f"SELECT * FROM poo.etudiant where class_ID='{choix_classe}'"
         res= db_management.query_from_DB(sql)
         print(res)
-
+        
+    # modifier les information personnelles d'un enseignant
     def changer_info(self,Enseignant_ID, column_input, val_input):
         sql=f"UPDATE poo.enseignant SET {column_input}='{val_input}' WHERE ID='{Enseignant_ID}'"
         db_management.insert_in_db(sql)
