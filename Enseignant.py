04cf3179f6829699246204f76fba79a47763aab0
REKKACHE Amira <56454854+MiraMimi1@users.noreply.github.com>
2022-12-04 21:08:25 +0100

    Update Enseignant.py

--- Enseignant.py
+++ Enseignant.py
@@ -32,30 +32,3 @@ class Enseignant (Personne):
         db_management.insert_in_db(sql)
         print("Personne modifiée")
         
-"""    
-           #modifier les informations d'un enseignant dans la base de données
-    def modifier_info(self,mail,numeroTel):
-        cnx = mysql.connector.connect(user='root', password='password', host='127.0.0.1', database='poo')
-        cursor = cnx.cursor()
-        cursor.execute("UPDATE enseignant.mail={}  , enseignant.numeroTel={} WHERE etudiant.etudiant_id=self.get_mmatricule ".format(numeroTel,mail))
-        cnx.commit()
-        cnx.close()
-        print("les étudiants de la meme classe")
-
-    #trouver un enseignant dans la base de donner à partir de matricule
-    @classmethod
-     def findByMatricul(self,mat):
-        cnx = mysql.connector.connect(user='root', password='password', host='127.0.0.1', database='poo')
-        cursor = cnx.cursor()
-        ens = cursor.execute("select enseignant where enseignant.enseignant_id=matr")
-        cnx.close()
-        return ens
-
-     def visualise_classe(self):
-        cnx = mysql.connector.connect(user='root', password='password', host='127.0.0.1', database='poo')
-        cursor = cnx.cursor()
-        ens = cursor.execute("select classroom_nom from enseignant join classroom join enseignement where enseignant.enseignant_id =enseignement.enseignant_id and enseignement.classroom_id = classroom.classroom_id and enseignant.enseignant_id=self.get_matricule")
-        cnx.close()
-        return ens
-
-"""
